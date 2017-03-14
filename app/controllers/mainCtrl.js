a5728ec2d550b7b15398f8843ffe586e99b01014
saibot777 <stefantrajkovic777@gmail.com>
2017-03-14 16:04:00 +0100

    code refactored, controllers created

---
+++ app/controllers/mainCtrl.js
@@ -0,0 +1,17 @@
+/**
+ * Created by stefan.trajkovic on 14.3.2017..
+ */
+function mainCtrl($scope) {
+    $scope.sortBy = 'name';
+    $scope.reverse = 'false';
+
+    $scope.customers=[
+        {joined: '2000-12-02', name: 'Stefan', city: 'Beograd', orderTotal: 9.9994},
+        {joined: '2011-10-02', name: 'Sladjana', city: 'Smederevo', orderTotal: 8.4994},
+        {joined: '2007-11-02', name: 'Jovan', city: 'Smederevo', orderTotal: 7.7994}
+    ]
+    $scope.doSort = function(propName) {
+        $scope.sortBy = propName;
+        $scope.reverse = !$scope.reverse;
+    };
+}
